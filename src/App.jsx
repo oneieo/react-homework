cff2d762084539255c7cf10575970355e6e19f8d
oneieo <sun020716@gmail.com>
2024-05-27 17:48:32 +0900

    update: done homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import styled from "styled-components";
 import "./App.css";
 
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Button = styled.button`
+  width: 60px;
+  height: 30px;
+  font-size: 15px;
+  border-radius: 10px;
+  margin: 0px 10px 0px 10px;
+  border: none;
+  background-color: ${(prop) => prop.$bgColor};
+  color: #ffffff;
+`;
+
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+  const handlePlusBtn = () => {
+    setCount(count + 1);
+  };
+  const handleMinusBtn = () => {
+    setCount(count - 1);
+  };
+  const handleResetBtn = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <Box>
+        <h1 className="count">Count: {count}</h1>
+        <div>
+          <Button $bgColor="#ff3434" onClick={handlePlusBtn}>
+            +
+          </Button>
+          <Button $bgColor="#2550ff" onClick={handleMinusBtn}>
+            -
+          </Button>
+          <Button $bgColor="#228b1e" onClick={handleResetBtn}>
+            reset
+          </Button>
+        </div>
+      </Box>
+    </>
+  );
 }
 
 export default App;
